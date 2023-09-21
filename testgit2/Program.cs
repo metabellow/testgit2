615588904e4b6605b39fef3df3d3a2d2433afe3d
mbrusanov <mbrusanov@outlook.com>
2023-09-21 15:14:33 +0700

    function

--- testgit2/Program.cs
+++ testgit2/Program.cs
@@ -14,14 +14,26 @@ namespace testgit2
             Console.Write("Введите число: ");
             int n = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine(fack(n));
+            Console.WriteLine($"Факториал числа {n}: {Fack(n)}");
+            Console.WriteLine($"Расчет функции от {n}. Вариант 1: {Function(n)}");
             Console.ReadKey();
         }
 
-            public static int fack(int n)
-            {
-                if (n == 1) return 1;
-                else return n * fack(n - 1);
-            }
+        public static int Fack(int n)
+        {
+            if (n == 1) return 1;
+            else return n * Fack(n - 1);
+        }
+
+        public static double Function(double n)
+        {
+             double res = Math.Sqrt(Math.Log(4.0/3.0)) + n + (9.0 / 7.0) - Math.Exp(Math.Sin((1.3 * n) - 0.7));
+
+
+            return res;
+        }
+
+
+
     }
 }

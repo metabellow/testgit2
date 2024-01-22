7e0bbcfb750337d0e124acb60e99d9e072b53fe1
mbrusanov <mbrusanov@outlook.com>
2024-01-22 12:35:23 +0700

    ALL LABS

--- testgit2/Program.cs
+++ testgit2/Program.cs
@@ -16,87 +16,13 @@ namespace testgit2
             Console.Write("Введите число: ");
             int n = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine($"Факториал числа {n}: {Factorial(n)}");
-            Console.WriteLine($"Расчет функции при значении x = {n}. Вариант 1: {Function(n)}");
-            Fibonachi(n);
-            Taylor();
+            TaylorSin taylor = new TaylorSin(n);
+
+            Console.WriteLine($"Факториал числа {taylor.N}: {TaylorSin.Factorial(n)}");
+            Console.WriteLine($"Расчет функции при значении x = {n}. Вариант 1: {TaylorSin.Function(n)}");
+            TaylorSin.Fibonachi(n);
+            TaylorSin.Taylor(n);
             Console.ReadKey();
         }
-
-        public static double Factorial(int n)
-        {
-            if (n == 0)
-                return 1;
-            return n * Factorial(n - 1);
-
-        }
-
-        public static double Function(double n)
-        {
-            double res = Math.Sqrt(Math.Log(4.0 / 3.0)) + n + (9.0 / 7.0) - Math.Exp(Math.Sin((1.3 * n) - 0.7));
-
-
-            return res;
-        }
-
-        public static void Fibonachi(int n)
-        {
-            int a = 0;
-            int b = 1;
-
-            Console.WriteLine($"Последовательность Фибоначчи до {n}: ");
-
-            for (int i = 0; i < n + 1; i++)
-            {
-                if (a <= n)
-                {
-                    Console.Write(a + " ");
-                    int temp = a;
-                    a = b;
-                    b = temp + b;
-                }
-                else { break; }
-
-
-            }
-
-            Console.WriteLine();
-        }
-
-        public static void Taylor()
-        {     
-            double n = 1.0;
-
-            Console.WriteLine($"Разложение sin({n}) по ряду Тейлора");
-            Console.Write($"Введите точность после запятой: ");
-            int count = int.Parse(Console.ReadLine());
-
-
-            Console.WriteLine($"sin({n}) примерно равен: {CalculateSinTaylor(n, count)}");
-        }
-
-
-        public static double CalculateSinTaylor(double x, int n)
-        {
-            double result = 0.0;
-            int j = 0;
-            double raz;
-            double temp;
-            double eps = Math.Pow(10, -n);
-
-
-            do
-            {
-                temp = result;
-                int exp = 2 * j + 1;
-                double term = Math.Pow(-1, j) * Math.Pow(x, exp) / Factorial(exp);
-                result += term;
-                j++;
-                raz = Math.Abs(temp - result);
-            } while (raz > eps);
-
-            return result;
-        }
-
     }
 }
---
+++ testgit2/Taylor.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testgit2
+{
+    public class TaylorSin
+    {
+        public int N { get; set; }
+
+        public TaylorSin()
+        {
+            N = 1;
+        }
+        public TaylorSin(int n)
+        {
+            N = n;
+        }
+
+        public static double Factorial(int n)
+        {
+            if (n == 0)
+                return 1;
+            return n * Factorial(n - 1);
+
+        }
+
+        public static double Function(double n)
+        {
+            double res = Math.Sqrt(Math.Log(4.0 / 3.0)) + n + (9.0 / 7.0) - Math.Exp(Math.Sin((1.3 * n) - 0.7));
+
+
+            return res;
+        }
+
+        public static void Fibonachi(int n)
+        {
+            int a = 0;
+            int b = 1;
+
+            Console.WriteLine($"Последовательность Фибоначчи до {n}: ");
+
+            for (int i = 0; i < n + 1; i++)
+            {
+                if (a <= n)
+                {
+                    Console.Write(a + " ");
+                    int temp = a;
+                    a = b;
+                    b = temp + b;
+                }
+                else { break; }
+
+
+            }
+
+            Console.WriteLine();
+        }
+
+        public static void Taylor(int nn)
+        {
+            double n = Convert.ToDouble(nn);
+
+            Console.WriteLine($"Разложение sin({n}) по ряду Тейлора");
+            Console.Write($"Введите точность после запятой: ");
+            int count = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine($"sin({n}) примерно равен: {CalculateSinTaylor(n, count)}");
+        }
+
+
+        public static double CalculateSinTaylor(double x, int n)
+        {
+            double result = 0.0;
+            int j = 0;
+            double raz;
+            double temp;
+            double eps = Math.Pow(10, -n);
+
+
+            do
+            {
+                temp = result;
+                int exp = 2 * j + 1;
+                double term = Math.Pow(-1, j) * Math.Pow(x, exp) / Factorial(exp);
+                result += term;
+                j++;
+                raz = Math.Abs(temp - result);
+            } while (raz > eps);
+
+            return result;
+        }
+    }
+}
